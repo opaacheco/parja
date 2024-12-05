bb4401f1a9c31edb2bfb03399b7a1b5e19f89c9a
Artiom Gusanu <artyy.gus@gmail.com>
2024-12-05 16:23:45 +0000

    substituicao echo's por heredoc

--- pages/menu.php
+++ pages/menu.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root"; 
-$password = "root";
+$password = "root123";
 $dbname = "lolja"; 
 
 
@@ -26,37 +26,48 @@ $sql = "SELECT id, nome, preco, foto_url FROM produtos";
 $result = mysqli_query($conn, $sql);
 
 function displayNoAuthPage(){
-  echo "<form method='POST' action=''>";
-  echo "<input type='email' name='email' placeholder='Email' required/>";
-  echo "<input type='password' name='password' placeholder='Senha' required/>";
-  echo "<button type='submit'>entrar</button>";
-  echo "</form>";
+  echo <<<HTML
+    <form method='POST' action=''>
+      <input type='email' name='email' placeholder='Email' required/>
+      <input type='password' name='password' placeholder='Senha' required/>
+      <button type='submit'>entrar</button>
+    </form>
 
-  echo "<form method='POST' action=''>";
-  echo "<input type='hidden' name='registar' value='true' />";
-  echo "<button type='submit'>registar</button>";
-  echo "</form>";
+    <form method='POST' action=''>
+      <input type='hidden' name='registar' value='true' />
+      <button type='submit'>registar</button>
+    </form>
+  HTML;
 }
 
 function displayRegister(){
-  echo "<form method='POST' action=''>";
-  echo "<input type='text' name='name' placeholder='Nome' required/>";
-  echo "<input type='email' name='email' placeholder='Email' required/>";
-  echo "<input type='password' name='password' placeholder='Senha' required/>";
-  echo "<button type='submit' name='registarBD'>Registar</button>";
-  echo "</form>";
+  echo <<<HTML
+    <form method='POST' action=''>
+      <input type='text' name='name' placeholder='Nome' required/>
+      <input type='email' name='email' placeholder='Email' required/>
+      <input type='password' name='password' placeholder='Senha' required/>
+      <button type='submit' name='registarBD'>Registar</button>
+    </form>
+  HTML;
 }
 
 function displayProdutos($result) {
   if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-      echo "<div class='produto' onclick=\"window.location.href='detalhes_produto.php?id=" . $row["id"] . "'\">";
-      echo "<h1>" . $row["nome"] . "</h1>";
-      echo "<img src='../".$row["foto_url"]."' alt='Imagem do Produto' width='200'/><br>";
-      echo "<input type='hidden' name='". $row["id"]."'>";
-      echo "<h2>€" . $row["preco"]."</h2>";
-      echo "<button>adicionar</button>";
-      echo "</div>";
+      $id = $row["id"];
+            $nome = htmlspecialchars($row["nome"]);
+            $fotoUrl = htmlspecialchars($row["foto_url"]);
+            $preco = htmlspecialchars($row["preco"]);
+
+      echo <<<HTML
+        <div class='produto' onclick="window.location.href='detalhes_produto.php?id=$id'">
+          <h1>$nome</h1>
+          <img src='../$fotoUrl' alt='Imagem do Produto' width='200'/><br>
+          <input type='hidden' name='$id'>
+          <h2>€$preco</h2>
+          <button>adicionar</button>
+        </div>
+      HTML;
     }
   } else {
       echo "<h1>Nenhum resultado encontrado.</h1>";
